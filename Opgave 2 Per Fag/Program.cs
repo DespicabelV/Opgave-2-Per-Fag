a7f61d23af14777895f3242a26920b8448c8b90a
Viggo Lysdahl <Viggo_lysdahl@hotmail.com>
2019-02-13 11:29:57 +0100

    Add project files.

---
+++ Opgave 2 Per Fag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opgave_2_Per_Fag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
